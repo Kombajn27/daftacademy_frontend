bf819910519c965b09396827c085746603d21da5
Kombajn27 <arielbiernacki@gmail.com>
2020-04-24 18:22:11 +0200

    Add responsivity and remove bootstrap

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import './styles/style.scss'
 // Import scripts
 import './scripts/carousel';
 import './scripts/img_import';
+
+// Import functions
+import hamburgerFunction from './scripts/hamburger';
+
+hamburgerFunction();
